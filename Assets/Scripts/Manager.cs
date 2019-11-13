3932bbfb337c7a366710f13838b4869966fb2039
jaisharma06 <jai.sharma06@live.com>
2019-11-14 02:09:43 +0530

    init commit

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Color lightTankColor;
+    public Color heavyTankColor;
+
+    [Header("Range")]
+    public int minX;
+    public int maxX;
+    public int minY;
+    public int maxY;
+
+    public Tank lightTankPrefab;
+    public Tank heavyTankPrefab;
+
+    public int heavyTankCount;
+    public int lightTankCount;
+    public Transform tanksParent;
+
+    void Start()
+    {
+        SpawnTanks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTanks()
+    {
+        for (int i = 0; i < heavyTankCount; i++)
+        {
+            Tank t = Instantiate(heavyTankPrefab, tanksParent);
+            int x = Random.Range(minX, maxX + 1);
+            int y = Random.Range(minY, maxY + 1);
+
+            t.transform.position = new Vector3(x, t.transform.position.y, y);
+        }
+
+        for (int i = 0; i < lightTankCount; i++)
+        {
+            Tank t = Instantiate(lightTankPrefab, tanksParent);
+
+            int x = Random.Range(minX, maxX + 1);
+            int y = Random.Range(minY, maxY + 1);
+
+            t.transform.position = new Vector3(x, t.transform.position.y, y);
+        }
+    }
+}
---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    [SerializeField]
+    private TankType type;
+    public float speed = 1;
+
+    private int hitCount = 2;
+    private Rigidbody rb;
+
+    public bool isActive = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (!isActive)
+            return;
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (verticalInput != 0 || horizontalInput != 0)
+        {
+            if (rb)
+            {
+                Vector3 velocity = new Vector3(horizontalInput * Time.deltaTime, 0, verticalInput * Time.deltaTime) * speed;
+                if (verticalInput != 0)
+                {
+                    velocity.x = 0;
+                }
+                else
+                {
+                    velocity.z = 0;
+                }
+                
+                rb.velocity = velocity;
+                transform.rotation = Quaternion.LookRotation(velocity, Vector3.up);
+            }
+        }
+        else
+        {
+            transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), transform.position.y, Mathf.RoundToInt(transform.position.z));
+        }
+    }
+
+    public void TakeHit()
+    {
+        hitCount--;
+        if (hitCount <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+
+    }
+}
